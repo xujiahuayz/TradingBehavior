61a7932c2238a6234e2058ab3130100ffa0fe010
sofiya-hub <69193875+sofiya-hub@users.noreply.github.com>
2020-08-12 11:55:02 +0200

    Add files via upload

--- GenderEffect/returns.R
+++ GenderEffect/returns.R
@@ -9,6 +9,8 @@ library(forcats)
 library(dplyr)
 library(hrbrthemes)
 library(ggpubr)
+install.packages("gtools")
+library(gtools)
 
 file<- "/home//qam/return_set15_19.csv"
 returns <- fread(file)
@@ -16,51 +18,127 @@ returns <- fread(file)
 file2<- "/home//qam/citrix/gender_label.csv"
 gender<- fread(file2)
 
-unique(returns)
+
 load("/home/qam/utrans2_2001_2019_reduced.rda")
 setnames(utrans2, "date_trans", "close_date")
 setnames(utrans2, "cur", "currency")
 utrans2[,close_date := as.character(close_date,format="%Y-%m-%d")]
-test1<- unique(utrans2[utrans2$close_date >= "2015-01-01",])
+
+### data on buy 
+
+#from 2015 to 2018
+
+test1<- unique(utrans2[utrans2$action == "B" & utrans2$close_date >= "2015-01-01",])
 
 test1.na<- na.omit(test1)
-dt_merge<- merge(test1.na, gender, by = c("client"), all.y = T)
+dt_merge<- merge(test1.na, gender, by = c("client"), all.x = T)
 unique(dt_merge)
 dt_mergeNew<-na.omit(dt_merge)
-test2<- unique(returns[returns$close_date <= "2019-09-27",])
-test3<-na.omit(test2)
 
-dt_mergeTot<- merge(dt_mergeNew, test3, by= c("close_date",  "stock_exchange"), all.y=TRUE)
+test2<-returns[returns$close_date <="2018-02-01",]
 
+
+##### 
+dt_mergeNew[,isin_code:=substr(isin,1,12)]
+dt_mergeNew[,stock_key:=paste(isin_code,currency,sep='_')]
 gc()
 
-tmp2 <- merge(dt_mergeNew,  test3, by = c("security_key", "close_date"), all.x = TRUE)
-tmp2.no.na<- na.omit(tmp2)
+tmp2 <- merge(dt_mergeNew,test2, by = c("stock_key", "close_date"), all.y= TRUE)
+tmp2.no.na <- drop_na(tmp2, title_label)
+
+## from 2018
+
+test3<- unique(utrans2[utrans2$action == "B" & utrans2$close_date >= "2018-02-01" ,])
+test3.na<- na.omit(test3)
+dt_merge2<- merge(test3.na, gender, by = c("client"), all.x = T)
+unique(dt_merge2)
+dt_mergeNew2<-na.omit(dt_merge2)
+test4<-returns[returns$close_date >="2018-02-01 ",]
+tmp3 <- merge(dt_mergeNew2,  test4, by = c("security_key", "close_date"), all.y= TRUE)
+tmp3.no.na <- na.omit(tmp3)
+
+#total set
+
+total_data <-rbind.fill(tmp2.no.na, tmp3.no.na)
 
 # regression on future returns over a day
+# tmp2= 2015-2018
+#tmp2 = 2018-2019
+#total_data = 2015-2019
 
-model<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp2.no.na)
-summary(model)
+model1<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp2.no.na)
+model2<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model3<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data)
+summary(model1)
+summary(model2)
+summary(model3)
+model1<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_70 + factor(title_label)*ret_measure_70, data = tmp2.no.na)
+model2<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_70 + factor(title_label)*ret_measure_70, data = tmp3.no.na)
+model3<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_70 + factor(title_label)* ret_measure_70, data = total_data)
+summary(model1)
+summary(model2)
+summary(model3)
 
 # regression on future returns over 5 days
 
-model2<- lm(fut_ret_measure_5 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp2.no.na)
-summary(model2)
+model4<- lm(fut_ret_measure_5 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp2.no.na)
+model5<- lm(fut_ret_measure_5 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model6<- lm(fut_ret_measure_5 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data)
+summary(model4)
+summary(model5)
+summary(model6)
+model4<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_70 + factor(title_label)*ret_measure_70, data = tmp2.no.na)
+model5<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_70 + factor(title_label)*ret_measure_70, data = tmp3.no.na)
+model6<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_70 + factor(title_label)* ret_measure_70, data = total_data)
+summary(model4)
+summary(model5)
+summary(model6)
 
 # regression on future returns over 25 days
 
-model3<- lm(fut_ret_measure_25 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = tmp2.no.na)
-summary(model3)
+model7<- lm(fut_ret_measure_25 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = tmp2.no.na)
+model8<- lm(fut_ret_measure_25 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model9<- lm(fut_ret_measure_25 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data)
+summary(model7)
+summary(model8)
+summary(model9)
+model7<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_70 + factor(title_label)*ret_measure_70, data = tmp2.no.na)
+model8<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_70 + factor(title_label)*ret_measure_70, data = tmp3.no.na)
+model9<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_70 + factor(title_label)* ret_measure_70, data = total_data)
+summary(model7)
+summary(model8)
+summary(model9)
+
 
 #regression on future retunrs over a 70 day period
 
-model4<- lm(fut_ret_measure_70~ factor(title_label) + ret_measure_250 + factor(title_label) * ret_measure_250, data = tmp2.no.na)
-summary(model4)
+model10<- lm(fut_ret_measure_70~ factor(title_label) + ret_measure_250 + factor(title_label) * ret_measure_250, data = tmp2.no.na)
+model11<- lm(fut_ret_measure_70 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model12<- lm(fut_ret_measure_70 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data)
+summary(model10)
+summary(model11)
+summary(model12)
 
+model10<- lm(fut_ret_measure_70~ factor(title_label) + ret_measure_70 + factor(title_label) * ret_measure_70, data = tmp2.no.na)
+model11<- lm(fut_ret_measure_70 ~ factor(title_label) + ret_measure_70 + factor(title_label)*ret_measure_70, data = tmp3.no.na)
+model12<- lm(fut_ret_measure_70 ~ factor(title_label) + ret_measure_70 + factor(title_label)* ret_measure_70, data = total_data)
+summary(model10)
+summary(model11)
+summary(model12)
 #regression on future returns over a 250 day period
 
-model5<-lm(fut_ret_measure_250 ~ factor(title_label) + ret_measure_250 + factor(title_label) * ret_measure_250, data = tmp2.no.na)
-summary(model5)
+model13<-lm(fut_ret_measure_250 ~ factor(title_label) + ret_measure_250 + factor(title_label) * ret_measure_250, data = tmp2.no.na)
+model14<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model15<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data)
+summary(model13)
+summary(model14)
+summary(model15)
+model13<-lm(fut_ret_measure_250 ~ factor(title_label) + ret_measure_70 + factor(title_label) * ret_measure_70, data = tmp2.no.na)
+model14<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_70 + factor(title_label)*ret_measure_70, data = tmp3.no.na)
+model15<- lm(fut_ret_measure_1 ~ factor(title_label) + ret_measure_70 + factor(title_label)* ret_measure_70, data = total_data)
+summary(model13)
+summary(model14)
+summary(model15)
 
 #graphs on returns 25
 
@@ -131,3 +209,211 @@ t.test(comFemme, comHomme,
        conf.level = 0.95)
 median(comFemme)
 median(comHomme)
+
+##### 
+utrans2[,isin_code:=substr(isin,1,12)]
+utrans2[,stock_key:=paste(isin_code,currency,sep='_')]
+merge(utrans2_temp,returns,by.x )
+
+### testing regressions on disposition effect 
+
+# regression on past returns over a day
+
+model1<- lm(ret_measure_1 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp2.no.na)
+model2<- lm(ret_measure_1 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model3<- lm(ret_measure_1 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data)
+summary(model1)
+summary(model2)
+summary(model3)
+
+# regression on past returns over 5 days
+
+model4<- lm(ret_measure_5 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp2.no.na)
+model5<- lm(ret_measure_5 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model6<- lm(ret_measure_5 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data)
+summary(model4)
+summary(model5)
+summary(model6)
+
+# regression on past returns over 25 days
+
+model7<- lm(ret_measure_25 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data)
+model8<- lm(ret_measure_25 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model9<- lm(ret_measure_25 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data)
+summary(model7)
+summary(model8)
+summary(model9)
+#regression on past retunrs over a 70 day period
+
+model10<- lm(fut_ret_measure_70~ factor(title_label) + ret_measure_250 + factor(title_label) * ret_measure_250, data = tmp2.no.na)
+model11<- lm(fut_ret_measure_70 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model12<- lm(fut_ret_measure_70 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data)
+summary(model10)
+summary(model11)
+summary(model12)
+
+### data on sales
+
+test2<- unique(utrans2[utrans2$action == "S" & utrans2$close_date >= "2015-01-01",])
+
+test2.na<- na.omit(test2)
+dt_merge<- merge(test2.na, gender, by = c("client"), all.x = T)
+unique(dt_merge)
+dt_mergeNew<-na.omit(dt_merge)
+
+test3<-returns[returns$close_date <="2018-02-01",]
+
+
+##### 
+dt_mergeNew[,isin_code:=substr(isin,1,12)]
+dt_mergeNew[,stock_key:=paste(isin_code,currency,sep='_')]
+gc()
+
+tmp2 <- merge(dt_mergeNew,test3, by = c("stock_key", "close_date"), all.y= TRUE)
+tmp2.no.na <- drop_na(tmp2, title_label)
+
+## from 2018
+
+test4<- unique(utrans2[utrans2$action == "S" & utrans2$close_date >= "2018-02-01" ,])
+test4.na<- na.omit(test4)
+dt_merge2<- merge(test4.na, gender, by = c("client"), all.x = T)
+unique(dt_merge2)
+dt_mergeNew2<-na.omit(dt_merge2)
+test5<-returns[returns$close_date >="2018-02-01 ",]
+tmp3 <- merge(dt_mergeNew2,  test5, by = c("security_key", "close_date"), all.y= TRUE)
+tmp3.no.na <- na.omit(tmp3)
+
+#total set
+
+total_data3 <-rbind.fill(tmp2.no.na, tmp3.no.na)
+
+### testing regressions on sales
+
+# regression on past returns over a day
+
+model1<- lm(ret_measure_1 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp2.no.na)
+model2<- lm(ret_measure_1 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model3<- lm(ret_measure_1 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data3)
+summary(model1)
+summary(model2)
+summary(model3)
+
+# regression on past returns over 5 days
+
+model4<- lm(ret_measure_5 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp2.no.na)
+model5<- lm(ret_measure_5 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model6<- lm(ret_measure_5 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data3)
+summary(model4)
+summary(model5)
+summary(model6)
+
+# regression on past returns over 25 days
+
+model7<- lm(ret_measure_25 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = tmp2.no.na)
+model8<- lm(ret_measure_25 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model9<- lm(ret_measure_25 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data3)
+summary(model7)
+summary(model8)
+summary(model9)
+#regression on past retunrs over a 70 day period
+
+model10<- lm(fut_ret_measure_70~ factor(title_label) + ret_measure_250 + factor(title_label) * ret_measure_250, data = tmp2.no.na)
+model11<- lm(fut_ret_measure_70 ~ factor(title_label) + ret_measure_250 + factor(title_label)*ret_measure_250, data = tmp3.no.na)
+model12<- lm(fut_ret_measure_70 ~ factor(title_label) + ret_measure_250 + factor(title_label)* ret_measure_250, data = total_data3)
+summary(model10)
+summary(model11)
+summary(model12)
+
+
+#### regressing using only the sign of returns for buys
+
+
+testR<-returns[returns$close_date <="2018-02-01",]
+
+##### 
+dt_mergeNew[,isin_code:=substr(isin,1,12)]
+dt_mergeNew[,stock_key:=paste(isin_code,currency,sep='_')]
+gc()
+
+tmpR <- merge(dt_mergeNew,testR, by = c("stock_key", "close_date"), all.y= TRUE)
+tmpR.no.na <- drop_na(tmpR, title_label)
+
+## from 2018
+
+test3<- unique(utrans2[utrans2$action == "B" & utrans2$close_date >= "2018-02-01" ,])
+test3.na<- na.omit(test3)
+dt_merge2<- merge(test3.na, gender, by = c("client"), all.x = T)
+unique(dt_merge2)
+dt_mergeNew2<-na.omit(dt_merge2)
+testR2<-returns[returns$close_date >="2018-02-01 ",]
+tmpR3 <- merge(dt_mergeNew2,  testR2, by = c("security_key", "close_date"), all.y= TRUE)
+tmpR3.no.na <- na.omit(tmpR3)
+
+#total set
+
+total_data4 <-rbind.fill(tmpR.no.na, tmpR3.no.na)
+newTotalData<- copy(total_data4)
+
+# changing values to 0's and one's
+
+ x = c(newTotalData$ret_measure_250)
+ x[x>0] <-1
+ x[x<=0] <-0
+
+ x<-x
+ 
+ z = c(newTotalData$ret_measure_70)
+ z[z>0] <-1
+ z[z<=0]<-0
+ 
+ z<- z
+ 
+ ### adding these vectors to the data set
+ 
+ new.df <- data.frame(
+   +  x,
+   + z)
+ 
+ dataSet <- (cbind(newTotalData, new.df))
+ 
+ 
+ ### regressions over 1 day
+ 
+model3<- lm(fut_ret_measure_1 ~ factor(title_label) + x + factor(title_label)* x, data = dataSet)
+summary(model3)
+model3<- lm(fut_ret_measure_1 ~ factor(title_label) + z + factor(title_label)* z, data = dataSet)
+summary(model3)
+
+# regression on future returns over 5 days
+
+
+model6<- lm(fut_ret_measure_5 ~ factor(title_label) + x + factor(title_label)* x, data = dataSet)
+summary(model6)
+
+model6<- lm(fut_ret_measure_5~ factor(title_label) + z+factor(title_label)* z, data = dataSet)
+summary(model6)
+
+# regression on future returns over 25 days
+
+
+model9<- lm(fut_ret_measure_25 ~ factor(title_label) + x+ factor(title_label)*x, data = dataSet)
+summary(model9)
+
+model9<- lm(fut_ret_measure_25~ factor(title_label) + z+ factor(title_label)* z, data = dataSet)
+summary(model9)
+
+#regression on future retunrs over a 70 day period
+
+
+model12<- lm(fut_ret_measure_70 ~ factor(title_label) + x+ factor(title_label)* x,data = dataSet)
+summary(model12)
+model12<- lm(fut_ret_measure_70 ~ factor(title_label) + z+ factor(title_label)* z,data = dataSet)
+summary(model12)
+
+#regression on future returns over a 250 day period
+
+
+model15<- lm(fut_ret_measure_250~factor(title_label) + x+ factor(title_label)* x,data = dataSet)
+summary(model15)
+model15<- lm(fut_ret_measure_250~factor(title_label) + z+ factor(title_label)* z,data = dataSet)
+summary(model15)
