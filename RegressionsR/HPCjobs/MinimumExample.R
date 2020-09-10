15e667101c7d2140f740857f688282fa58e25d97
xujiahuayz <31636867+xujiahuayz@users.noreply.github.com>
2020-09-10 20:02:28 +0100

    add minimum example

---
+++ RegressionsR/HPCjobs/MinimumExample.R
@@ -0,0 +1,10 @@
+library(data.table)
+
+datapath = '../../data/'
+outputpath = '../../output/'
+
+insiders = fread(paste0(datapath, 'insiders_accounts.txt'),
+                 sep = ";", header = T, col.names = c(
+                   "client", "attr_id", "attr_val"))
+
+write.csv(insiders, file = paste0(outputpath,"minimumexample.csv"))
