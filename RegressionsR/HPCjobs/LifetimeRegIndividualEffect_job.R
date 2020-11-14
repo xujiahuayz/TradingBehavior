281efaac2a0f0e73883fd5cd9f62f1491b40675a
xujiahuayz <31636867+xujiahuayz@users.noreply.github.com>
2020-11-14 15:26:00 +0000

    add

--- RegressionsR/HPCjobs/LifetimeRegIndividualEffect_job.R
+++ RegressionsR/HPCjobs/LifetimeRegIndividualEffect_job.R
@@ -29,24 +29,28 @@ temp0 = xx[is.finite(rreturn365),
                         ), by = account_date]
 
 
-pickage = c(2, 3.5, 5) * 364.2425 # pick an age
+agerange = list(
+  c(1, 2),
+  c(2, 3.5),
+  c(3.5, 5)
+)
 
 trselsplm = list()
 ns = c()
-for (k in 1:length(pickage)){
-  
-  i = pickage[k]
+for (k in 1:length(agerange)){
+  pickage = agerange[[k]] * 365.2425
+  i = pickage[2]
   popu = regtable[lifetime >= i & firstobs >= '2009-01-01']$client
   temp = temp0[age <= i & client %in% popu]
   ns = c(ns, temp$client %>% unique() %>% length())
   gc()
-  
+  temp[, agediff = age - pickage[1]]
   trselsplm[[k]] = pdata.frame(
-    temp[, c('client', 'account_date', 'age', 'retpr')], 
+    temp[, c('client', 'account_date', 'agediff', 'retpr')], 
     index=c('client', 'account_date'), 
     row.names = F
     )
-  trsel = temp[, c(n=.N, funcmeanconf(retpr)), by = age][order(age)]
+  trsel = temp[, c(n=.N, funcmeanconf(retpr)), by = agediff][order(agediff)]
   trselscum[[k]] = trsel
   
   print(k)
@@ -54,68 +58,69 @@ for (k in 1:length(pickage)){
 
 
 #exited after two years and before 5
-popu = regtable[lifetime >= 2 * 364.2425 & lifetime <= 5 * 364.2425 & 
+popu = regtable[lifetime >= 2 * 365.2425 & lifetime <= 5 * 365.2425 & 
                   is.finite(closing_date) & firstobs >= '2009-01-01']$client
 temp = temp0[client %in% popu]
+temp[, agediff = age - i]
 
 trselsplm[[k+1]] = pdata.frame(
-  temp[, c('client', 'account_date', 'age', 'retpr')], 
+  temp[, c('client', 'account_date', 'agediff', 'retpr')], 
   index=c('client', 'account_date'), 
   row.names = F
   )
 
 gc()
-trsel = temp[age < 2 * 364.2425, c(n=.N,funcmeanconf(retpr)), by = age][order(age)]
+trsel = temp[age < 2 * 365.2425, c(n=.N,funcmeanconf(retpr)), by = agediff][order(agediff)]
 trselscum[[k+1]] = trsel
 ns = c(ns, temp$client %>% unique() %>% length())
 
 
 time = Sys.time()
 # when age is aligned, using plm/lm seems not make a difference 
-xx11t = plm(retpr~age, data = trselsplm[[1]][trselsplm[[1]]$age < 365.2425*1.5,], effect = 'individual')
+xx11t = plm(retpr~age, data = trselsplm[[1]][trselsplm[[1]]$agediff < 365.2425*1.5,], effect = 'individual')
 xx11t %>% fixef %>% t.test
 summary(xx11t)
 Sys.time() - time
 
 time = Sys.time()
-xx21t = plm(retpr~age, data = trselsplm[[2]][trselsplm[[2]]$age < 365.2425*1.5,], effect = 'individual')
+xx21t = plm(retpr~age, data = trselsplm[[2]][trselsplm[[2]]$agediff < 365.2425*1.5,], effect = 'individual')
 xx21t %>% fixef %>% t.test
 summary(xx21t)
 Sys.time() - time
 
-xx31t = plm(retpr~age, data = trselsplm[[3]][trselsplm[[3]]$age < 365.2425*1.5,], effect = 'individual')
+xx31t = plm(retpr~age, data = trselsplm[[3]][trselsplm[[3]]$agediff < 365.2425*1.5,], effect = 'individual')
 xx31t %>% fixef %>% t.test
 summary(xx31t)
 
-xx41t = plm(retpr~age, data = trselsplm[[4]][trselsplm[[4]]$age < 365.2425*1.5,], effect = 'individual')
+xx41t = plm(retpr~age, data = trselsplm[[4]][trselsplm[[4]]$agediff < 365.2425*1.5,], effect = 'individual')
 xx41t %>% fixef %>% t.test
 summary(xx41t)
 
-xx12t = plm(retpr~age, data = trselsplm[[1]][trselsplm[[1]]$age >= 365.2425*1.5 & trselsplm[[1]]$age < 365.2425*2,], effect = 'individual')
+xx12t = plm(retpr~age, data = trselsplm[[1]][trselsplm[[1]]$agediff >= 365.2425*1.5 & trselsplm[[1]]$age < 365.2425*2,], effect = 'individual')
 xx12t %>% fixef %>% t.test
 summary(xx12t)
 
-xx22t = plm(retpr~age, data = trselsplm[[2]][trselsplm[[2]]$age >= 365.2425*1.5 & trselsplm[[2]]$age < 365.2425*2,], effect = 'individual')
+xx22t = plm(retpr~age, data = trselsplm[[2]][trselsplm[[2]]$agediff >= 365.2425*1.5 & trselsplm[[2]]$age < 365.2425*2,], effect = 'individual')
 xx22t %>% fixef %>% t.test
 summary(xx22t)
 
-xx32t = plm(retpr~age, data = trselsplm[[3]][trselsplm[[3]]$age >= 365.2425*1.5 & trselsplm[[3]]$age < 365.2425*2,], effect = 'individual')
+xx32t = plm(retpr~age, data = trselsplm[[3]][trselsplm[[3]]$agediff >= 365.2425*1.5 & trselsplm[[3]]$age < 365.2425*2,], effect = 'individual')
 xx32t %>% fixef %>% t.test
 summary(xx32t)
 
-xx42t = plm(retpr~age, data = trselsplm[[4]][trselsplm[[4]]$age >= 365.2425*1.5 & trselsplm[[4]]$age < 365.2425*2,], effect = 'individual')
+xx42t = plm(retpr~age, data = trselsplm[[4]][trselsplm[[4]]$agediff >= 365.2425*1.5 & trselsplm[[4]]$age < 365.2425*2,], effect = 'individual')
 xx42t %>% fixef %>% t.test
 summary(xx42t)
 
-xx23t = plm(retpr~age, data = trselsplm[[2]][trselsplm[[2]]$age >= 365.2425*2 & trselsplm[[2]]$age < 365.2425*3.5,], effect = 'individual')
-xx23t %>% fixef %>% t.test
-summary(xx23t)
+# xx23t = plm(retpr~age, data = trselsplm[[2]][trselsplm[[2]]$age >= 365.2425*2 & trselsplm[[2]]$age < 365.2425*3.5,], effect = 'individual')
+# xx23t %>% fixef %>% t.test
+# summary(xx23t)
+# 
+# xx33t = plm(retpr~age, data = trselsplm[[3]][trselsplm[[3]]$age >= 365.2425*2 & trselsplm[[3]]$age < 365.2425*3.5,], effect = 'individual')
+# xx33t %>% fixef %>% t.test
+# summary(xx33t)
 
-xx33t = plm(retpr~age, data = trselsplm[[3]][trselsplm[[3]]$age >= 365.2425*2 & trselsplm[[3]]$age < 365.2425*3.5,], effect = 'individual')
-xx33t %>% fixef %>% t.test
-summary(xx33t)
-
-xx34t = plm(retpr~age, data = trselsplm[[3]][trselsplm[[3]]$age >= 365.2425*3.5 & trselsplm[[3]]$age < 365.2425*5,], effect = 'individual')
+xx34t = plm(retpr~age, data = trselsplm[[3]][trselsplm[[3]]$agediff >= 365.2425*2 & trselsplm[[3]]$age < 365.2425*5,], effect = 'individual')
 xx34t %>% fixef %>% t.test
 summary(xx34t)
 
---
+++ RegressionsR/HPCjobs/LongevityIndividualEffect_job.R
@@ -0,0 +1,112 @@
+library(data.table)
+library(plm)
+library(magrittr)
+library(stats)
+
+getwd()
+
+funcmeanconf = function(series){
+  list(mret = mean(series, na.rm = T),
+       conf1 = t.test(series)$conf.int[1],
+       conf2 = t.test(series)$conf.int[2])
+}
+
+filerootpath = '/home/jxu/TradingBehavior/'
+# filerootpath = '../'
+filepath = paste0(filerootpath, 'data/')
+
+load(paste0(filepath,'regtable.rda'))
+load(paste0(filepath,'tsregressreg.rda'))
+xx[, ':='(firstobs.y = NULL)]
+
+trselscum = list()
+
+temp0 = xx[is.finite(rreturn365),
+           list(
+             client= client,
+             account_date = account_date,
+             ret =  rreturn365,
+             age = age)][, ':='(
+               retpr = ecdf(ret)(ret) # rank of return
+               # retpr = scale(ret) %>% as.numeric()
+             ), by = account_date]
+
+
+pickage = c(2, 3.5, 5) * 365.2425 # pick an age
+
+trselsplm = list()
+ns = c()
+for (k in 1:length(pickage)){
+  
+  i = pickage[k]
+  popu = regtable[lifetime >= i & firstobs >= '2009-01-01']$client
+  temp = temp0[age <= i & client %in% popu]
+  ns = c(ns, temp$client %>% unique() %>% length())
+  gc()
+  
+  trselsplm[[k]] = pdata.frame(
+    temp[, c('client', 'account_date', 'age', 'retpr')], 
+    index=c('client', 'account_date'), 
+    row.names = F
+  )
+  trsel = temp[, c(n=.N, funcmeanconf(retpr)), by = age][order(age)]
+  trselscum[[k]] = trsel
+  
+  print(k)
+}
+
+agerange = list(
+  c(1, 1.5),
+  c(1.5, 2),
+  c(2, 5)
+  )
+
+
+print(0 %>% paste0('====================================================='))
+
+
+for (k in 1:length(agerange)){
+  pickage = agerange[[k]] * 365.2425
+  popu = regtable[lifetime >= pickage[2] & firstobs >= '2009-01-01', c('lifetime', 'client')]
+  
+  # popu[, ':='(cohort = floor(as.numeric(lifetime/(182.62125/6))))] # cohorting in lifetime in half year
+  
+  # select only observations where clients are between that age bin
+  # temp = merge(
+  #   temp0[age > pickage[k] & age <= pickage[k+1]], popu[, c('cohort', 'client')], by = 'client'
+  #   )[ , list(
+  #     retpr = mean(retpr, na.rm = T),
+  #     account_date = mean(account_date, na.rm = T),
+  #     n = length(retpr)
+  #     #have to do a synthetic group, otherwise `pvcm` does not run
+  #     ), by = c('cohort', 'age')][n > 200]
+  
+  temp = merge(
+    temp0[age > pickage[1] & age <= pickage[2]], popu[, c('lifetime', 'client')], by = 'client'
+  )
+  
+  temp[ , ':='(
+    agediff = as.numeric(age - pickage[1]),
+    lifediff = as.numeric(lifetime - pickage[2])
+    )]
+
+  temp %>% head %>% print
+  
+  gw <- lm(retpr ~ lifediff + agediff + I(lifediff * agediff) , data = temp
+            # , effect = 'individual', index = c("client", "account_date")
+           )
+  # gw[['coefficients']] %>% print
+  gw %>% summary %>% print
+  # 
+  # cor.test(gw[['coefficients']][,1], gw[['coefficients']][,2]) %>% print
+  # cor.test(gw[['coefficients']][,1], gw[['coefficients']] %>% row.names() %>% as.numeric()) %>% print
+  # cor.test(gw[['coefficients']] %>% row.names() %>% as.numeric(), gw[['coefficients']][,2]) %>% print
+  #
+  gc() 
+  print(agerange[[k]] %>% paste0('====================================================='))
+}
+
+
+
+
+
