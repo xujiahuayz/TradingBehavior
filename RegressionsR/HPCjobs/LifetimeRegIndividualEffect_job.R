9cfb2c4164f286d131a95bb05a0fb140a8fc3f4a
xujiahuayz <31636867+xujiahuayz@users.noreply.github.com>
2020-11-14 15:39:03 +0000

    update

--- RegressionsR/HPCjobs/LifetimeRegIndividualEffect_job.R
+++ RegressionsR/HPCjobs/LifetimeRegIndividualEffect_job.R
@@ -44,15 +44,13 @@ for (k in 1:length(agerange)){
   temp = temp0[age <= i & client %in% popu]
   ns = c(ns, temp$client %>% unique() %>% length())
   gc()
-  temp[, agediff = age - pickage[1]]
   trselsplm[[k]] = pdata.frame(
-    temp[, c('client', 'account_date', 'agediff', 'retpr')], 
+    temp[, c('client', 'account_date', 'age', 'retpr')], 
     index=c('client', 'account_date'), 
     row.names = F
     )
-  trsel = temp[, c(n=.N, funcmeanconf(retpr)), by = agediff][order(agediff)]
+  trsel = temp[, c(n=.N, funcmeanconf(retpr)), by = age][order(age)]
   trselscum[[k]] = trsel
-  
   print(k)
 }
 
@@ -61,54 +59,53 @@ for (k in 1:length(agerange)){
 popu = regtable[lifetime >= 2 * 365.2425 & lifetime <= 5 * 365.2425 & 
                   is.finite(closing_date) & firstobs >= '2009-01-01']$client
 temp = temp0[client %in% popu]
-temp[, agediff = age - i]
 
 trselsplm[[k+1]] = pdata.frame(
-  temp[, c('client', 'account_date', 'agediff', 'retpr')], 
+  temp[, c('client', 'account_date', 'age', 'retpr')], 
   index=c('client', 'account_date'), 
   row.names = F
   )
 
 gc()
-trsel = temp[age < 2 * 365.2425, c(n=.N,funcmeanconf(retpr)), by = agediff][order(agediff)]
+trsel = temp[age < 2 * 365.2425, c(n=.N,funcmeanconf(retpr)), by = age][order(age)]
 trselscum[[k+1]] = trsel
 ns = c(ns, temp$client %>% unique() %>% length())
 
 
 time = Sys.time()
 # when age is aligned, using plm/lm seems not make a difference 
-xx11t = plm(retpr~age, data = trselsplm[[1]][trselsplm[[1]]$agediff < 365.2425*1.5,], effect = 'individual')
+xx11t = plm(retpr~I(age-365.2425*1), data = trselsplm[[1]][trselsplm[[1]]$age < 365.2425*1.5,], effect = 'individual')
 xx11t %>% fixef %>% t.test
 summary(xx11t)
 Sys.time() - time
 
 time = Sys.time()
-xx21t = plm(retpr~age, data = trselsplm[[2]][trselsplm[[2]]$agediff < 365.2425*1.5,], effect = 'individual')
+xx21t = plm(retpr~I(age-365.2425*1), data = trselsplm[[2]][trselsplm[[2]]$age < 365.2425*1.5,], effect = 'individual')
 xx21t %>% fixef %>% t.test
 summary(xx21t)
 Sys.time() - time
 
-xx31t = plm(retpr~age, data = trselsplm[[3]][trselsplm[[3]]$agediff < 365.2425*1.5,], effect = 'individual')
+xx31t = plm(retpr~I(age-365.2425*1), data = trselsplm[[3]][trselsplm[[3]]$age < 365.2425*1.5,], effect = 'individual')
 xx31t %>% fixef %>% t.test
 summary(xx31t)
 
-xx41t = plm(retpr~age, data = trselsplm[[4]][trselsplm[[4]]$agediff < 365.2425*1.5,], effect = 'individual')
+xx41t = plm(retpr~I(age-365.2425*1), data = trselsplm[[4]][trselsplm[[4]]$age < 365.2425*1.5,], effect = 'individual')
 xx41t %>% fixef %>% t.test
 summary(xx41t)
 
-xx12t = plm(retpr~age, data = trselsplm[[1]][trselsplm[[1]]$agediff >= 365.2425*1.5 & trselsplm[[1]]$age < 365.2425*2,], effect = 'individual')
+xx12t = plm(retpr~I(age-365.2425*1.5), data = trselsplm[[1]][trselsplm[[1]]$age >= 365.2425*1.5 & trselsplm[[1]]$age < 365.2425*2,], effect = 'individual')
 xx12t %>% fixef %>% t.test
 summary(xx12t)
 
-xx22t = plm(retpr~age, data = trselsplm[[2]][trselsplm[[2]]$agediff >= 365.2425*1.5 & trselsplm[[2]]$age < 365.2425*2,], effect = 'individual')
+xx22t = plm(retpr~I(age-365.2425*1.5), data = trselsplm[[2]][trselsplm[[2]]$age >= 365.2425*1.5 & trselsplm[[2]]$age < 365.2425*2,], effect = 'individual')
 xx22t %>% fixef %>% t.test
 summary(xx22t)
 
-xx32t = plm(retpr~age, data = trselsplm[[3]][trselsplm[[3]]$agediff >= 365.2425*1.5 & trselsplm[[3]]$age < 365.2425*2,], effect = 'individual')
+xx32t = plm(retpr~I(age-365.2425*1.5), data = trselsplm[[3]][trselsplm[[3]]$age >= 365.2425*1.5 & trselsplm[[3]]$age < 365.2425*2,], effect = 'individual')
 xx32t %>% fixef %>% t.test
 summary(xx32t)
 
-xx42t = plm(retpr~age, data = trselsplm[[4]][trselsplm[[4]]$agediff >= 365.2425*1.5 & trselsplm[[4]]$age < 365.2425*2,], effect = 'individual')
+xx42t = plm(retpr~I(age-365.2425*1.5), data = trselsplm[[4]][trselsplm[[4]]$age >= 365.2425*1.5 & trselsplm[[4]]$age < 365.2425*2,], effect = 'individual')
 xx42t %>% fixef %>% t.test
 summary(xx42t)
 
@@ -120,7 +117,7 @@ summary(xx42t)
 # xx33t %>% fixef %>% t.test
 # summary(xx33t)
 
-xx34t = plm(retpr~age, data = trselsplm[[3]][trselsplm[[3]]$agediff >= 365.2425*2 & trselsplm[[3]]$age < 365.2425*5,], effect = 'individual')
+xx34t = plm(retpr~I(age-365.2425*2), data = trselsplm[[3]][trselsplm[[3]]$age >= 365.2425*2 & trselsplm[[3]]$age < 365.2425*5,], effect = 'individual')
 xx34t %>% fixef %>% t.test
 summary(xx34t)
 
